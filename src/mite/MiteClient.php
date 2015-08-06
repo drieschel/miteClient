350d79827fca4eba84ca2201df0abb40d4e8e4d6
Immanuel Klinkenberg <klinkenberg@speicher-werk.de>
2015-08-06 11:39:49 +0200

    MiteClient rudimentary created.

--- src/mite/MiteClient.php
+++ src/mite/MiteClient.php
@@ -6,7 +6,7 @@ namespace drieschel\mite;
 class MiteClient
 {
   /**
-   * @var \Pest
+   * @var \PestJSON
    */
   protected $pest;
   
@@ -21,18 +21,81 @@ class MiteClient
   protected $baseUrl;
   
   /**
-   * @param string $apiKey
-   * @param string $baseUrl
+   * @var string
    */
-  function __construct($apiKey, $baseUrl = '')
+  protected $responseFormat = 'json';
+  
+  /**
+   * @param string $apiKey
+   * @param string $miteUrl
+   * @param string $responseFormat
+   */
+  function __construct($apiKey, $miteUrl, $responseFormat = 'json')
   {
     $this->apiKey = $apiKey;
-    $this->baseUrl = $baseUrl;
+    $this->baseUrl = $miteUrl;
+    
+    if(!in_array($responseFormat, array('json')))
+    {
+      throw new \Exception($responseFormat . ' is not a valid response format');
+    }    
+    $this->responseFormat = $responseFormat;    
+    $this->pest = new \PestJSON($miteUrl);
   }
   
+  /**
+   * @param integer $id
+   * @return mixed
+   */
   public function getCustomer($id)
   {
-    return $this->pest->
+    return $this->get('customers/' . $id);
+  }
+  
+  /**
+   * @param integer $id
+   * @return mixed
+   */
+  public function getProject($id)
+  {
+    return $this->get('projects/' . $id);
+  }
+  
+  /**
+   * @param array $parameters
+   * @return mixed
+   */
+  public function getProjects(array $parameters = array())
+  {
+    return $this->get('projects', $parameters);
+  }
+  
+  /**
+   * @param array $parameters
+   * @return mixed
+   */
+  public function getTimeEntriesBy(array $parameters)
+  {
+    return $this->get('time_entries', $parameters);
+  }
+  
+  /**
+   * @param integer $projectId
+   * @return mixed
+   */
+  public function getTimeEntriesByProject($projectId)
+  {
+    return $this->getTimeEntriesBy(array('project_id' => $projectId));    
   }
 
+  /**
+   * @param string $url
+   * @param array $data
+   * @param array $headers
+   */
+  protected function get($url, array $data = array(), array $headers = array())
+  {
+    $data['api_key'] = $this->apiKey;
+    return $this->pest->get($url . '.' . $this->responseFormat, $data, $headers);
+  }
 }
